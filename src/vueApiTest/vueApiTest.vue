612ea3cd22b29658aaedd532efae0054c6fd4ed2
hjai <hjaiim@163.com>
2018-09-21 17:50:28 +0800

    input 自动获取焦点

--- src/vueApiTest/vueApiTest.vue
+++ src/vueApiTest/vueApiTest.vue
@@ -2,12 +2,15 @@
   <main-layout>
     <filter-test></filter-test>
     <Mixin-test></Mixin-test>
+    <!-- 自动获得焦点 -->
+    <input-test></input-test>
   </main-layout>
 </template>
 <script type="text/ecmascript-6">
 import MainLayout from "common/mainLayout";
 import FilterTest from "./testApi/filters";
 import MixinTest from "./testApi/mixin.vue";
+import InputTest from "./testApi/input.vue";
 export default {
   created() {},
   data() {
@@ -16,7 +19,8 @@ export default {
   components: {
     MainLayout,
     FilterTest,
-    MixinTest
+    MixinTest,
+    InputTest
   },
 
   watch: {},
